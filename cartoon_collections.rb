d4ccde62ff213665a0a5e989b24d24f0764b5908
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 05:26:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,19 +1,17 @@
 def roll_call_dwarves dwarves
   dwarves.each {|dwarf| puts dwarf}
-end                                  # => :roll_call_dwarves
+end
 
 def summon_captain_planet calls
   calls.map {|call| call.capitalize + "!"}
-end                                         # => :summon_captain_planet
+end
 
 def long_planeteer_calls calls
   calls.any? {|call| call.size > 4}
-end                                  # => :long_planeteer_calls
+end
 
 def find_the_cheese strings
   cheese_types = ["cheddar", "gouda", "camembert"]
   strings.find {|string| cheese_types.include? string}
-end                                                 # => :find_the_cheese
+end
 
-
-"hi there".include? "i t"  # => true
